6d84120ba2fe8bb74ee5921a13eb45bae8517a00
16chuang <16chuang@castilleja.org>
2015-07-06 23:17:16 -0700

    Added custom table view cell classes for donor cell and donation cell. Also added UI components to each cell with basic auto layout. (Cancelled view on donation cell is uninstalled at the moment to hide it.)

---
+++ Mezzo-1.0/Mezzo-1.0/Views/Donations/DonationTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  DonationTableViewCell.swift
+//  Mezzo-1.0
+//
+//  Created by Claire Huang on 7/6/15.
+//  Copyright (c) 2015 MezzoAwesomeness. All rights reserved.
+//
+
+import UIKit
+
+class DonationTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var timeAndLocationLabel: UILabel!
+    @IBOutlet weak var cancelledView: UIView!
+}
