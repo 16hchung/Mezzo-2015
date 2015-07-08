d61fe2cac76b95d1776db4056abd612723f03bc9
Heejung Chung <16hchung@castilleja.org>
2015-07-07 20:36:14 -0700

    added basic outline of the Donor class

---
+++ Mezzo-1.0/Mezzo-1.0/Models/Donor.swift
@@ -0,0 +1,55 @@
+//
+//  Donor.swift
+//  Mezzo-1.0
+//
+//  Created by Heejung Chung on 7/7/15.
+//  Copyright (c) 2015 MezzoAwesomeness. All rights reserved.
+//
+
+import Foundation
+import Parse
+
+class Donor: PFObject, PFSubclassing {
+    
+    // MARK: from Parse
+    
+    /// login info from Parse (don't need to use often)
+    @NSManaged var user: PFUser?
+    /// access `location` property instead
+    @NSManaged var locatedAt: PFGeoPoint?
+    /// identification for organizations and donors
+    @NSManaged var EIN: String
+    @NSManaged var businessName: String
+    
+    // MARK: Methods
+    
+    // TODO: create Donation/Org classes + uncomment => implement
+    /**
+        Creates new `Donation` object and sends out offer
+        
+        :param: atTime suggested time of donation's pickup
+        :param: toOrganization organization receiving the offer
+    */
+//    func offerADonation(atTime: NSDate, toOrganization: Organization) {
+//        
+//    }
+    
+    
+    // MARK: PFSubclassing Protocol
+    
+    static func parseClassName() -> String {
+        return "Donor"
+    }
+    
+    override init() {
+        super.init()
+    }
+    
+    override class func initialize() {
+        var onceToken : dispatch_once_t = 0;
+        dispatch_once(&onceToken) {
+            // inform Parse about this subclass
+            self.registerSubclass()
+        }
+    }
+}
