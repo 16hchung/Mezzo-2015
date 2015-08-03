1d51d467ad7b87fcb1de8961d0841b0f3fb7c269
16chuang <16chuang@castilleja.org>
2015-08-03 15:36:50 -0700

    Add keyboard handling and scrolling for my info settings page.
    Added done toolbar to each of the text inputs. Scroll view scrolls automatically if the first responder is currently the special instructions text view.

--- Mezzo-1.0/Mezzo-1.0/View Controllers/Settings/MyInfoViewController.swift
+++ Mezzo-1.0/Mezzo-1.0/View Controllers/Settings/MyInfoViewController.swift
@@ -7,13 +7,36 @@
 //
 
 import UIKit
+import ConvenienceKit
 
 class MyInfoViewController: UIViewController {
+    
+    @IBOutlet weak var managerName: UITextField!
+    @IBOutlet weak var phoneNumber: UITextField!
+    @IBOutlet weak var specialInstructions: TextView!
+    @IBOutlet weak var scrollView: UIScrollView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // add border to text view
+        specialInstructions.layer.borderWidth = 1.0
+        let grayColor = UIColor(white: 0.875, alpha: 1.000)
+        specialInstructions.layer.borderColor = grayColor.CGColor
+//        specialInstructions.layer.borderColor = UIColor.blackColor().CGColor
+        specialInstructions.layer.cornerRadius = 5.0
+        
+        addDoneToKeyboard() // add done toolbar
+        
+        registerForKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +44,63 @@ class MyInfoViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK: keyboard handling
+    
+    private func registerForKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWasShown:", name: UIKeyboardDidShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillBeHidden:", name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func keyboardWasShown(notif: NSNotification) {
+        if let info = notif.userInfo {
+            let keyboardSize = info[UIKeyboardFrameBeginUserInfoKey]?.CGRectValue().size
+            let contentInset = UIEdgeInsetsMake(0.0, 0.0, keyboardSize!.height + 50, 0.0)
+            scrollView.contentInset = contentInset
+            scrollView.scrollIndicatorInsets = contentInset
+            
+            // if currently editing special instructions text view, scroll up
+            if specialInstructions.isFirstResponder() {
+                var rect = self.view.frame
+                rect.size.height -= keyboardSize!.height
+                if !CGRectContainsPoint(rect, CGPointMake(specialInstructions.frame.origin.x, specialInstructions.frame.origin.y + 100)) {
+                    let scrollPoint = CGPointMake(0.0, -(specialInstructions.frame.origin.y - keyboardSize!.height))
+                    scrollView.setContentOffset(scrollPoint, animated: true)
+                }
+            }
+        }
+    }
+    
+    func keyboardWillBeHidden(notif: NSNotification) {
+        let contentInset = UIEdgeInsetsZero
+        
+        scrollView.contentInset = contentInset
+        scrollView.scrollIndicatorInsets = contentInset
+        
+        scrollView.setContentOffset(CGPointMake(0.0, -self.view.frame.origin.y / 2), animated: true)
+    }
+    
+    private func addDoneToKeyboard() {
+        var doneToolbar: UIToolbar = UIToolbar(frame: CGRectMake(0, 0, 320, 50))
+        doneToolbar.barStyle = UIBarStyle.Default
+        
+        var flexSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.FlexibleSpace, target: nil, action: nil)
+        var done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.Done, target: self, action: Selector("doneButtonAction"))
+        
+        var items = NSMutableArray()
+        items.addObject(flexSpace)
+        items.addObject(done)
+        
+        doneToolbar.items = items as [AnyObject]
+        doneToolbar.sizeToFit()
+        
+        specialInstructions.inputAccessoryView = doneToolbar
+        phoneNumber.inputAccessoryView = doneToolbar
+        managerName.inputAccessoryView = doneToolbar
+    }
+    
+    func doneButtonAction() {
+        self.view.endEditing(true)
+    }
 
     /*
     // MARK: - Navigation
