58048de81e44cc2f099a4eebe66e8789766cffea
16chuang <16chuang@castilleja.org>
2015-08-03 11:16:14 -0700

    Add rudimentary badge count (cloud code increments count; count clears completely when app is opened).

--- Mezzo-1.0/Mezzo-1.0/AppDelegate.swift
+++ Mezzo-1.0/Mezzo-1.0/AppDelegate.swift
@@ -139,6 +139,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        
+        // clear the badge count when the app is about to be launched
+        let installation = PFInstallation.currentInstallation()
+        if (installation.badge > 0) {
+            installation.badge = 0
+            installation.saveEventually()
+        }
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
