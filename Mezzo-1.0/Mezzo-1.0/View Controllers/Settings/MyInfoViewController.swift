c3f2fa34f4dfcfe51cca3179cc0e17f95c7a8ea5
16chuang <16chuang@castilleja.org>
2015-08-03 22:32:46 -0700

    Add phone number keyboard for my info settings page.

--- Mezzo-1.0/Mezzo-1.0/View Controllers/Settings/MyInfoViewController.swift
+++ Mezzo-1.0/Mezzo-1.0/View Controllers/Settings/MyInfoViewController.swift
@@ -15,6 +15,8 @@ class MyInfoViewController: UIViewController {
     @IBOutlet weak var phoneNumber: UITextField!
     @IBOutlet weak var specialInstructions: TextView!
     @IBOutlet weak var scrollView: UIScrollView!
+    
+    private var specialInstructionsActive: Bool = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,12 +29,16 @@ class MyInfoViewController: UIViewController {
         specialInstructions.layer.borderWidth = 1.0
         let grayColor = UIColor(white: 0.875, alpha: 1.000)
         specialInstructions.layer.borderColor = grayColor.CGColor
-//        specialInstructions.layer.borderColor = UIColor.blackColor().CGColor
         specialInstructions.layer.cornerRadius = 5.0
         
         addDoneToKeyboard() // add done toolbar
         
-        registerForKeyboardNotifications()
+        // register for keyboard show/hide notifications
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWasShown:", name: UIKeyboardDidShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillBeHidden:", name: UIKeyboardWillHideNotification, object: nil)
+        
+        phoneNumber.delegate = self // set text field delegate
+        specialInstructions.delegate = self // set text view delegate
     }
     
     override func viewWillDisappear(animated: Bool) {
@@ -44,12 +50,9 @@ class MyInfoViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    // MARK: keyboard handling
     
-    private func registerForKeyboardNotifications() {
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWasShown:", name: UIKeyboardDidShowNotification, object: nil)
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillBeHidden:", name: UIKeyboardWillHideNotification, object: nil)
-    }
+    
+    // MARK: keyboard handling
     
     func keyboardWasShown(notif: NSNotification) {
         if let info = notif.userInfo {
@@ -59,7 +62,7 @@ class MyInfoViewController: UIViewController {
             scrollView.scrollIndicatorInsets = contentInset
             
             // if currently editing special instructions text view, scroll up
-            if specialInstructions.isFirstResponder() {
+            if specialInstructionsActive {
                 var rect = self.view.frame
                 rect.size.height -= keyboardSize!.height
                 if !CGRectContainsPoint(rect, CGPointMake(specialInstructions.frame.origin.x, specialInstructions.frame.origin.y + 100)) {
@@ -79,6 +82,7 @@ class MyInfoViewController: UIViewController {
         scrollView.setContentOffset(CGPointMake(0.0, -self.view.frame.origin.y / 2), animated: true)
     }
     
+    /// Adds done toolbar to the keyboards of all three text inputs.
     private func addDoneToKeyboard() {
         var doneToolbar: UIToolbar = UIToolbar(frame: CGRectMake(0, 0, 320, 50))
         doneToolbar.barStyle = UIBarStyle.Default
@@ -98,18 +102,66 @@ class MyInfoViewController: UIViewController {
         managerName.inputAccessoryView = doneToolbar
     }
     
+    /// Dismisses keyboard when done toolbar button is tapped.
     func doneButtonAction() {
         self.view.endEditing(true)
     }
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+extension MyInfoViewController: UITextFieldDelegate {
+    
+    /// Formats phone number string while user is typing
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        if textField == phoneNumber {
+            var newString = (textField.text as NSString).stringByReplacingCharactersInRange(range, withString: string)
+            // take out any non-number text
+            var components = newString.componentsSeparatedByCharactersInSet(NSCharacterSet.decimalDigitCharacterSet().invertedSet)
+            var decimalString = "".join(components) as NSString
+            
+            var length = decimalString.length
+            var hasLeadingOne = length > 0 && decimalString.characterAtIndex(0) == (1 as unichar)
+            
+            if length == 0 || (length > 10 && !hasLeadingOne) || length > 11 {
+                var newLength = (textField.text as NSString).length + (string as NSString).length - range.length as Int
+                return (newLength > 10) ? false : true
+            }
+            var index = 0 as Int
+            var formattedString = NSMutableString()
+            
+            if hasLeadingOne {
+                formattedString.appendString("1 ")
+                index += 1
+            }
+            
+            if (length - index) > 3 {
+                var areaCode = decimalString.substringWithRange(NSMakeRange(index, 3))
+                formattedString.appendFormat("(%@) ", areaCode)
+                index += 3
+            }
+            
+            if length - index > 3 {
+                var prefix = decimalString.substringWithRange(NSMakeRange(index, 3))
+                formattedString.appendFormat("%@-", prefix)
+                index += 3
+            }
+            
+            var remainder = decimalString.substringFromIndex(index)
+            formattedString.appendString(remainder)
+            textField.text = formattedString as String
+            return false
+        } else {
+            return true
+        }
     }
-    */
+}
 
+extension MyInfoViewController: UITextViewDelegate {
+    func textViewDidBeginEditing(textView: UITextView) {
+        self.specialInstructionsActive = true
+    }
+    
+    func textViewDidEndEditing(textView: UITextView) {
+        self.specialInstructionsActive = false
+    }
 }
