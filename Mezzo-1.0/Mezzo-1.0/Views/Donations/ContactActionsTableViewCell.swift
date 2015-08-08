c67d6cc047bfec88bb559fa7fbcfade38b415a98
Heejung Chung <16hchung@castilleja.org>
2015-08-07 17:10:38 -0700

    new tableViewCell classes created

---
+++ Mezzo-1.0/Mezzo-1.0/Views/Donations/ContactActionsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ContactActionsTableViewCell.swift
+//  Mezzo-1.0
+//
+//  Created by Heejung Chung on 8/7/15.
+//  Copyright (c) 2015 MezzoAwesomeness. All rights reserved.
+//
+
+import UIKit
+
+class ContactActionsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
