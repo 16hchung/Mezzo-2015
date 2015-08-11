54ef893366dcae49cfc6b3c22a6f632df4dd9f49
16chuang <16chuang@castilleja.org>
2015-08-11 12:46:51 -0700

    Add UI and donation actions helper classes.

---
+++ Mezzo-1.0/Mezzo-1.0/View Controllers/DonationActionsHelper.swift
@@ -0,0 +1,13 @@
+//
+//  DonationActionsHelper.swift
+//  Mezzo-1.0
+//
+//  Created by Claire Huang on 8/11/15.
+//  Copyright (c) 2015 MezzoAwesomeness. All rights reserved.
+//
+
+import Foundation
+
+class DonationActionsHelper {
+    
+}
